6284bc4af10b5a309a039d3583f4bec0f52dd1ff
MaGixPro <dimah556@mail.ru>
2023-03-02 21:16:23 +0300

    Add workflow for push action.

--- recommendation_system/settings.py
+++ recommendation_system/settings.py
@@ -122,29 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-
-DB_USERNAME=os.environ.get("DB_USERNAME")
-DB_PASSWORD=os.environ.get("DB_PASSWORD")
-DB_HOST=os.environ.get("DB_HOST")
-DB_PORT=os.environ.get("DB_PORT")
-DB_DATABASE=os.environ.get("DB_DATABASE")
-DB_IS_AVAIL = all([
-        DB_USERNAME,
-        DB_PASSWORD,
-        DB_HOST,
-        DB_PORT,
-        DB_DATABASE
-])
-
-if DB_IS_AVAIL:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": DB_DATABASE,
-            "USER": DB_USERNAME,
-            "PASSWORD": DB_PASSWORD,
-            "HOST": DB_HOST,
-            "PORT": DB_PORT,
-        }
-    }
